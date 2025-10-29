23c6461eb912bf7e8e35fb99e5ad848931604a10
Yogasri29 <kurapatiyogasri4@gmail.com>
2025-10-29 13:34:34 +0530

    Add files via upload

---
+++ logical operator.cpp
@@ -0,0 +1,14 @@
+/*write a c program on logical operator*/
+#include <stdio.h>
+ int main()
+{
+int a = 5, b = 5, c = 10, result; result = (a == b) && (c > b);
+printf("(a == b) && (c > b) is %d \n", result); result = (a == b) && (c < b);
+printf("(a == b) && (c < b) is %d \n", result); result = (a == b) || (c < b);
+printf("(a == b) || (c < b) is %d \n", result); result = (a != b) || (c < b);
+printf("(a != b) || (c < b) is %d \n", result); result = !(a != b);
+
+printf("!(a != b) is %d \n", result); result = !(a == b);
+printf("!(a == b) is %d \n", result); return 0;
+}
+
