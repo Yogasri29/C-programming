6e7f44821bcf2a662eb932e007235b8bc60a7abc
Yogasri29 <kurapatiyogasri4@gmail.com>
2025-10-29 13:35:50 +0530

    Add files via upload

---
+++ relational operator.cpp
@@ -0,0 +1,20 @@
+/*write a c program on relational operator*/
+#include <stdio.h>
+int main() 
+{
+
+int a = 5, b = 5, c = 10;
+printf("%d == %d is %d \n", a, b, a == b); 
+printf("%d == %d is %d \n", a, c, a == c); 
+printf("%d > %d is %d \n", a, b, a > b);
+ printf("%d > %d is %d \n", a, c, a > c);
+
+printf("%d < %d is %d \n", a, b, a < b);
+ printf("%d < %d is %d \n", a, c, a < c);
+  printf("%d != %d is %d \n", a, b, a != b);
+printf("%d != %d is %d \n", a, c, a != c);
+ printf("%d >= %d is %d \n", a, b, a >= b);
+  printf("%d >= %d is %d \n", a, c, a >= c);
+   printf("%d <= %d is %d \n", a, b, a <= b);
+    printf("%d <= %d is %d \n", a, c, a <= b);
+}
