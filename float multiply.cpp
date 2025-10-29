07bfc2a93fc255d14b591e9f405af7c69ff0a3c5
Yogasri29 <kurapatiyogasri4@gmail.com>
2025-10-29 13:27:28 +0530

    Add files via upload

---
+++ float multiply.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{float num1,num2,product;
+printf("enter the first number:");
+scanf("%f", &num1);
+printf("enter the second number:");
+scanf("%f", &num2);
+	product=num1*num2;
+	printf("product of the entered numbers");
+}
+
