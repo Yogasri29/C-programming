c8a6c36db4c7759d4f73d6e94049b2b068bc4f64
Yogasri29 <kurapatiyogasri4@gmail.com>
2025-10-29 13:38:29 +0530

    Add files via upload

---
+++ square root.cpp
@@ -0,0 +1,12 @@
+/*write  a c program to find the sqare root of two numbers*/
+#include<stdio.h>
+#include <math.h> 
+
+int main() {
+    double number, root;
+
+    printf("Enter a number: ");
+    scanf("%lf", &number);
+    }
+
+
