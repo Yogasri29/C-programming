eed3a18f523d9c9d01329f6d076cdb668452af30
Yogasri29 <kurapatiyogasri4@gmail.com>
2025-11-04 07:56:32 +0530

    Add files via upload

--- area of the circle.cpp
+++ area of the circle.cpp
@@ -1,10 +1,12 @@
-/*WRITE A C PROGRAM TO FIND THE AREA OF CIRCLE*/
-#include<stdio.h>
-#define pi 3.14
+#include <stdio.h>
+#define PI 3.14
 int main()
-{float radius,area;
-printf("enter radius of the circle(in cm):");
-scanf("%f",&radius);
-area=pi*radius*radius;
-printf("area of the circle=%f cm",area);
+{
+ float radius, area;
+ printf("Enter Radius of Circle(in cm): ");
+ scanf("%f", &radius);
+ area = PI * radius * radius;
+ printf("Area of Circle = %.2f cm\n", area);
+ return 0;
 }
+
