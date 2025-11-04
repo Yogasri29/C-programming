c561f0867020711ce6ffd1e17216948ec064b7d3
Yogasri29 <kurapatiyogasri4@gmail.com>
2025-11-04 07:58:37 +0530

    Add files via upload

---
+++ average marks of a student in five subjects.cpp
@@ -0,0 +1,13 @@
+/*write a c program to find average marks of student in five subjects*/
+#include <stdio.h>
+int main()
+{
+int English, Maths, Science, Psychology, History,total;
+float average;
+printf("Enter the marks of English, Maths, Science, Psychology and History: \n");
+scanf("%d %d %d %d %d", &English, &Maths, &Science, &Psychology, &History);
+total = English + Maths + Science + Psychology + History;
+average = total / 5.0;
+printf("Total Marks of the Student: %d\n", total);
+printf("Average Marks of the Student: %.2f\n", average);
+}
